cbdd972d7c53400159ca4d0f4dfe4fcfcc2c024f
Kevin Most <kevinmost@users.noreply.github.com>
2016-11-07 12:34:44 -0500

    Print entire stacktrace on RetryException (#4)

--- src/main/java/com/kevinmost/junit_retry_rule/RetryException.java
+++ src/main/java/com/kevinmost/junit_retry_rule/RetryException.java
@@ -2,6 +2,9 @@ package com.kevinmost.junit_retry_rule;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 /**
  * An exception thrown to signal that a retry operation (executed via {@link RetryRule}) has retried more than the
  * allowed number of times, and has still failed.
@@ -17,12 +20,19 @@ public final class RetryException extends RuntimeException {
       final Throwable error = errors[i];
       msg.append('\n');
       msg.append("Attempt #").append(i).append(" threw exception:");
-      msg.append(error);
+      msg.append(stackTraceAsString(error));
     }
     return new RetryException(msg.toString());
   }
 
   private RetryException(@NotNull String message) {
     super(message);
+  }
+
+
+  @NotNull private static String stackTraceAsString(@NotNull Throwable t) {
+    final StringWriter errors = new StringWriter();
+    t.printStackTrace(new PrintWriter(errors));
+    return errors.toString();
   }
 }
